19af9a2e574ee166ff1391a4cab5f73d5c87be68
ignaciodemarchi <ignacio22demarchi@gmail.com>
2025-09-03 19:25:35 -0300

    Changes to client.go

--- client/common/client.go
+++ client/common/client.go
@@ -56,6 +56,7 @@ func (c *Client) createClientSocket() error {
 			c.config.ID,
 			err,
 		)
+		return err
 	}
 	c.conn = conn
 	return nil
@@ -66,16 +67,31 @@ func (c *Client) StartClientLoop() {
 	// There is an autoincremental msgID to identify every message sent
 	// Messages if the message amount threshold has not been surpassed
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
+
+		select {
+		case <-c.stopCh:
+			log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+			return
+		default:
+		}
+
 		// Create the connection the server in every loop iteration. Send an
 		c.createClientSocket()
 
-		// TODO: Modify the send to avoid short-write
-		fmt.Fprintf(
-			c.conn,
-			"[CLIENT %v] Message N°%v\n",
-			c.config.ID,
-			msgID,
-		)
+		// Write full line (avoid short-writes)
+		bw := bufio.NewWriter(c.conn)
+		line := fmt.Sprintf("[CLIENT %v] Message N°%v\n", c.config.ID, msgID)
+		if _, err := bw.WriteString(line); err != nil {
+			log.Errorf("action: send_message | result: fail | client_id: %v | error: %v", c.config.ID, err)
+			_ = c.conn.Close()
+			return
+		}
+		if err := bw.Flush(); err != nil {
+			log.Errorf("action: send_message | result: fail | client_id: %v | error: %v", c.config.ID, err)
+			_ = c.conn.Close()
+			return
+		}
+
 		msg, err := bufio.NewReader(c.conn).ReadString('\n')
 		c.conn.Close()
 
@@ -92,9 +108,17 @@ func (c *Client) StartClientLoop() {
 			msg,
 		)
 
-		// Wait a time between sending one message and the next one
-		time.Sleep(c.config.LoopPeriod)
-
+		// Interruptible sleep so SIGTERM doesn’t wait a full period
+		timer := time.NewTimer(c.config.LoopPeriod)
+		select {
+		case <-c.stopCh:
+			if !timer.Stop() {
+				<-timer.C
+			}
+			log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+			return
+		case <-timer.C:
+		}
 	}
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
 }
